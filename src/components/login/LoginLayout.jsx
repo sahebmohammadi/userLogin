6e88e96e8e7afe56857f1a2839df2f6ed2a62ac3
sahebmohammadi <saheb.put92@gmail.com>
2020-12-01 13:48:47 +0330

    loginLayout component added

---
+++ src/components/login/LoginLayout.jsx
@@ -0,0 +1,15 @@
+// imports
+import React from 'react';
+import classes from './loginLayout.module.scss';
+
+const loginLayout = ({ children }) => {
+  return (
+    <>
+      <div className={classes.container}>
+        <div className={classes.formBg}>{children}</div>
+      </div>
+    </>
+  );
+};
+
+export default loginLayout;
